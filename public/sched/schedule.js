06f836b61b1fbbba09c7616cf6bd9e2772352b8a
thealmightyderpybird <73889377+thealmighyderpybird@users.noreply.github.com>
2025-01-13 20:26:02 -0500

    go back to normal

--- public/sched/schedule.js
+++ public/sched/schedule.js
@@ -31,8 +31,7 @@ function createCBSHSched(element) {
     element.appendChild(application);
     application.id = "cbsh-application";
 
-    //fetch("https://api.croomssched.tech/today").then((res) => {
-    fetch("/today.json").then((res) => {
+    fetch("https://api.croomssched.tech/today").then((res) => {
         return res.text();
     }).then((res) => {
         return JSON.parse(res).data;
