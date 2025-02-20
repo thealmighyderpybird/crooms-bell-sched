f4cb3ec2d2e9152b563d71de627081ecdd250eff
Andrew Jennings <73889377+thealmighyderpybird@users.noreply.github.com>
2025-02-20 11:09:45 -0500

    update stuff

--- public/sched/settings/export.js
+++ public/sched/settings/export.js
@@ -1,7 +1,6 @@
 function exportSettings() {
     let settings = localStorage.getItem("settings");
     settings = JSON.stringify(settings);
-    console.log(settings);
 
     function download(text, name, type) {
         let file = new Blob([text], {type: type});
