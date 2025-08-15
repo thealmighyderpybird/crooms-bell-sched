c4d3b4a0bc3c2ffd2c4749c9d72981f4adcb2d5f
thealmightyderpybird <73889377+thealmighyderpybird@users.noreply.github.com>
2025-08-14 21:12:21 -0400

    new tos

---
+++ src/app/terms/page.tsx
@@ -0,0 +1,133 @@
+import Card from "~/components/index/Card";
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+    title: "Terms of Service",
+};
+
+export default function TermsOfService() {
+    return <Card>
+        <p style={{ marginBlockEnd: "0" }}><strong>Crooms Bell Schedule Terms of Service</strong></p>
+        <p>Effective Date: Friday, August 15th, 2025</p>
+        <h3>1. Introduction</h3>
+        <p>
+            Welcome to Crooms Bell Schedule! Our Terms of Service (&#34;Terms&#34;) govern your use of our services,
+            including but not limited to access to our platform, discussions, and community interactions.
+            By accessing or using our services, you agree to comply with these Terms.
+            If you have any questions or concerns, or if you feel excluded, please contact our administration lead,
+            Andrew, to recommend changes or improvements.
+        </p>
+        <h3>2. General User Rights</h3>
+        <p>
+            Regardless of the specific service or feature you are using, we provide the following basic rights to all
+            users:
+        </p>
+        <ul>
+            <li>Access our services without interruption, barring any technical issues.</li>
+            <li>Post content to and engage with the services.</li>
+            <li>Participate in meaningful, respectful, and entertaining discussions.</li>
+        </ul>
+        <p>
+            Please be aware that these rights may be revoked or restricted if you violate any provision of these Terms.
+        </p>
+        <h3>3. Conditional Rights</h3>
+        <p>
+            There are certain actions that may be deemed offensive or controversial and require prior approval from our
+            administration before being undertaken. Violations of these conditions may result in suspension or
+            termination of your access.
+        </p>
+        <p>Permitted with prior approval:</p>
+        <ul>
+            <li>Discussions of sensitive political topics.</li>
+            <li>Discussions of controversial issues.</li>
+            <li>Discussions related to unique habits of teachers.</li>
+        </ul>
+        <h3>4. Prohibited Activities</h3>
+        <p>
+            The following actions are strictly prohibited and may result in restrictions, suspension, or permanent bans
+            from Crooms Bell Schedule services:
+        </p>
+        <ul>
+            <li>
+                Failure to demonstrate basic kindness, respect, and consideration for others, regardless of their
+                background.
+            </li>
+            <li>
+                Advertising or promoting products/services without prior payment to the Crooms Bell Schedule
+                administration. For custom advertising inquiries, please contact our administration lead, Andrew.
+            </li>
+            <li>Use of slurs or offensive language.</li>
+            <li>Sharing explicit content or discussing personal sexual actions.</li>
+            <li>Leaking government documents or other confidential materials.</li>
+            <li>Providing instructions on how to create, obtain, or distribute illegal items.</li>
+            <li>
+                Engaging in spamming or attempting to disable or disrupt our services through methods such as
+                Distributed Denial of Service (DDoS) attacks.
+            </li>
+        </ul>
+        <h3>5. Admin Discernment Clause</h3>
+        <p>
+            While this Terms of Service document provides a comprehensive list of prohibited behaviors, moderators and
+            administrators reserve the right to take action against content or behavior that may not be explicitly
+            listed but is deemed harmful or disruptive. In such cases, administrators will remove the content or take
+            appropriate disciplinary action as they see fit.
+        </p>
+        <h3>6. Consequences for Violating the Terms of Service</h3>
+        <p>
+            We take violations of these Terms seriously. The following actions will be taken for violations, with
+            increasing severity depending on the nature of the offense:
+        </p>
+        <ul>
+            <li>
+                <strong>First Offense:</strong> You will receive a warning, unless the offense is severe enough to
+                warrant immediate suspension. In severe cases, we may skip the warning and move directly to a second
+                offense.
+            </li>
+            <li>
+                <strong>Second Offense:</strong> You will face a temporary account suspension, typically lasting one
+                day, though it may be extended depending on the violation&#39;s severity.
+            </li>
+            <li>
+                <strong>Third Offense:</strong> A more extended suspension or termination of your account may occur. In
+                cases of severe
+                violations,
+                system-wide bans or lockdowns may be implemented.
+            </li>
+        </ul>
+        <p>We also accept anonymous tips and reports of violations.</p>
+        <h3>7. Discord Server Rules</h3>
+        <p>
+            Our Discord server provides an additional platform for communication within the Crooms Bell Schedule
+            community.
+            In addition to the above Terms, we require the following behavior within the server:
+        </p>
+        <ul>
+            <li>
+                Post content in the appropriate channels. While there are exceptions, each case will be reviewed
+                individually.
+            </li>
+            <li>Keep serious discussions within the designated #serious-topics channel.</li>
+            <li>Keep political discussions within the designated #politics channel.</li>
+            <li>
+                We reserve the right to utilize additional tools, such as an entry buffer channel and a warning channel,
+                to ensure compliance with these Terms.
+            </li>
+        </ul>
+        <h3>8. Modifications to Terms</h3>
+        <p>
+            Crooms Bell Schedule reserves the right to modify or amend these Terms at any time. Any updates to these
+            Terms will be communicated, and continued use of our services constitutes your acceptance of the revised
+            Terms.
+        </p>
+        <h3>9. Contact Information</h3>
+        <p>
+            If you have any questions, concerns, or suggestions regarding our Terms of Service or wish to report a
+            violation, please contact our administration lead, Andrew, at
+            <a href="mailto:andrew@croomssched.tech">andrew@croomssched.tech</a>.
+        </p>
+        <p style={{ marginBlockEnd: "0" }}>
+            <strong>By using the Crooms Bell Schedule services, you acknowledge that you have read, understood, and
+                agree to be bound by these Terms of Service.</strong>
+        </p>
+    </Card>
+};
