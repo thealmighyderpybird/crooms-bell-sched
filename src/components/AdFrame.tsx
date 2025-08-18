2c62a51ec4ced13029f152537258f9235260780a
thealmightyderpybird <73889377+thealmighyderpybird@users.noreply.github.com>
2025-08-18 07:43:01 -0400

    better no-select rules

--- src/components/AdFrame.tsx
+++ src/components/AdFrame.tsx
@@ -1,5 +1,6 @@
+import styles from "~/app/index.module.css";
 import type { HTMLProps } from "react";
 
 export default function AdFrame(props: HTMLProps<HTMLIFrameElement>) {
-    return <iframe src="https://ad.croomssched.tech" height={175} {...props}></iframe>
+    return <iframe src="https://ad.croomssched.tech" height={175} className={ styles.ad } {...props}></iframe>
 };
--- src/components/cards/ProwlerWidget.tsx
+++ src/components/cards/ProwlerWidget.tsx
@@ -1,3 +1,4 @@
+import indexStyles from "../../app/index.module.css";
 import CardHeader from "../index/CardHeader";
 import Prowler from "~/prowler/root";
 import Card from "../index/Card";
@@ -6,7 +7,7 @@ import Link from "next/link";
 export default function ProwlerWidget() {
     return <Card>
         <CardHeader>Prowler</CardHeader>
-        <Link href="/prowler/post" style={{ marginBlockEnd: "1rem", display: "block" }}>Share a post</Link>
+        <Link href="/prowler/post" className={ indexStyles.sharePostLink }>Share a post</Link>
         <Prowler />
     </Card>;
 };
