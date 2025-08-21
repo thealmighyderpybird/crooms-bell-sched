e03cdac373d834922c95b62a621bc9c1dfa69da8
Andrew Jennings <73889377+thealmighyderpybird@users.noreply.github.com>
2025-08-21 11:44:33 -0400

    eason 2

--- src/components/cards/ProwlerWidget.tsx
+++ src/components/cards/ProwlerWidget.tsx
@@ -1,13 +1,16 @@
 import indexStyles from "../../app/index.module.css";
+import getSession from "~/lib/session.server";
 import CardHeader from "../index/CardHeader";
 import Prowler from "~/prowler/root";
 import Card from "../index/Card";
 import Link from "next/link";
 
-export default function ProwlerWidget() {
+export default async function ProwlerWidget() {
+    const { sid } = await getSession();
+
     return <Card>
         <CardHeader>Prowler</CardHeader>
         <Link href="/prowler/post" className={ indexStyles.sharePostLink }>Share a post</Link>
-        <Prowler />
+        <Prowler sid={sid} />
     </Card>;
 };
