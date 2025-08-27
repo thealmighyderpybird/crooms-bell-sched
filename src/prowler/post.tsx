fcbc2ae04863336b30e4a9c523bad42c0f832619
thealmightyderpybird <73889377+thealmighyderpybird@users.noreply.github.com>
2025-08-27 07:57:53 -0400

    update sanitizer

--- src/prowler/post.tsx
+++ src/prowler/post.tsx
@@ -39,6 +39,32 @@ export default function Post({ post }: { post: Post }) {
                 </span> }
             </div>
         </div>
-        <div className={ styles.corePostContent } dangerouslySetInnerHTML={{ __html: sanitizeHtml(post?.data) }} />
+        <div className={ styles.corePostContent } dangerouslySetInnerHTML={{ __html: sanitizeHtml(post?.data, {
+            allowedTags: [
+                "address", "h1", "h2", "h3", "h4", "h5", "h6", "hgroup", "blockquote", "dd", "div",
+                "dl", "dt", "figcaption", "figure", "hr", "li", "main", "ol", "p", "pre", "img",
+                "ul", "a", "abbr", "b", "bdi", "bdo", "br", "cite", "code", "data", "dfn",
+                "em", "i", "kbd", "mark", "q", "rb", "rp", "rt", "rtc", "ruby", "s", "samp",
+                "small", "span", "strong", "sub", "sup", "time", "u", "var", "wbr", "caption",
+                "col", "colgroup", "table", "tbody", "td", "tfoot", "th", "thead", "tr", "rainbow"
+            ],
+            disallowedTagsMode: 'discard',
+            allowedAttributes: {
+                a: [ 'href', 'name', 'target' ],
+                img: [ 'src', 'srcset', 'alt', 'title', 'width', 'height', 'loading' ],
+                '*': [ 'mention' ]
+            },
+            allowedClasses: {
+                a: [ 'links' ],
+            },
+            selfClosing: [ 'img', 'br', 'hr', 'area', 'base', 'basefont', 'input' ],
+            // URL schemes we permit
+            allowedSchemes: [ 'http', 'https', 'ftp', 'mailto', 'tel' ],
+            allowedSchemesByTag: {},
+            allowedSchemesAppliedToAttributes: [ 'href', 'src', 'cite' ],
+            allowProtocolRelative: true,
+            enforceHtmlBoundary: false,
+            parseStyleAttributes: true
+        }) }} />
     </div>;
 };
