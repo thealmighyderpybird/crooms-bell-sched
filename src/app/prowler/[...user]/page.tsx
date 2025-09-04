5cc74cbeb0a79dc15873cf7ef935463a16022e5a
Andrew Jennings <73889377+thealmighyderpybird@users.noreply.github.com>
2025-09-04 11:52:51 -0400

    camel

--- src/app/prowler/[...user]/page.tsx
+++ src/app/prowler/[...user]/page.tsx
@@ -1,7 +1,10 @@
+import UserCard from "~/app/prowler/[...user]/UserCard";
+
 export default async function Page({ params }: { params: Promise<{ user: string }> }) {
     const username = (await params).user;
     
     return <>
+        <UserCard username={username} />
         {username}
     </>;
 }
