8332b1c4c5e6dd3126c6c4979cee080b938cf4f5
thealmightyderpybird <73889377+thealmighyderpybird@users.noreply.github.com>
2025-09-12 08:29:41 -0400

    new sanitizer settings

--- src/lib/SanitizeContent.ts
+++ src/lib/SanitizeContent.ts
@@ -4,20 +4,27 @@ const SanitizeContent = (html: string) => sanitizeHtml(html, {
     allowedTags: [
         "address", "h1", "h2", "h3", "h4", "h5", "h6", "hgroup", "blockquote", "dd", "div",
         "dl", "dt", "figcaption", "figure", "hr", "li", "main", "ol", "p", "pre", "img",
-        "ul", "a", "abbr", "b", "bdi", "bdo", "br", "cite", "code", "data", "dfn",
-        "em", "i", "kbd", "mark", "q", "rb", "rp", "rt", "rtc", "ruby", "s", "samp",
-        "small", "span", "strong", "sub", "sup", "time", "u", "var", "wbr", "caption",
-        "col", "colgroup", "table", "tbody", "td", "tfoot", "th", "thead", "tr", "rainbow"
+        "ul", "a", "abbr", "b", "bdi", "bdo", "br", "cite", "code", "data", "dfn", "em",
+        "i", "kbd", "mark", "q", "rb", "rp", "rt", "rtc", "ruby", "s", "samp", "small",
+        "span", "strong", "sub", "sup", "time", "u", "var", "wbr", "caption", "col",
+        "colgroup", "table", "tbody", "td", "tfoot", "th", "thead", "tr", "rainbow"
     ],
     disallowedTagsMode: "discard",
     allowedAttributes: {
         a: [ "href", "name", "target" ],
         img: [ "src", "srcset", "alt", "title", "width", "height", "loading" ],
-        "*": [ "mention", "class" ]
+        "*": [ "mention", "style" ]
     },
     allowedClasses: {
+        span: [ "rainbow" ],
         a: [ "links" ],
-        "*": [ "rainbow" ],
+    },
+    allowedStyles: {
+        "*": {
+            "color": [/^#(0x)?[0-9a-f]+$/i, /^rgb\(\s*(\d{1,3})\s*,\s*(\d{1,3})\s*,\s*(\d{1,3})\s*\)$/],
+            "font-weight": [/^[1-9]00$/, /^bold$/, /^normal$/, /^light$/, /^bolder$/],
+            "font-style": [/^normal$/, /^italic$/, /^oblique$/, /^revert$/, /^unset$/],
+        },
     },
     selfClosing: [ "img", "br", "hr", "area", "base", "basefont", "input" ],
     // URL schemes we permit
@@ -26,7 +33,10 @@ const SanitizeContent = (html: string) => sanitizeHtml(html, {
     allowedSchemesAppliedToAttributes: [ "href", "src", "cite" ],
     allowProtocolRelative: true,
     enforceHtmlBoundary: false,
-    parseStyleAttributes: true
+    parseStyleAttributes: true,
+    parser: {
+        lowerCaseTags: true,
+    },
 });
 
 export default SanitizeContent;
