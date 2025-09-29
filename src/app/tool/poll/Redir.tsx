6f318d8a42015d39da26347d64e6eb3021a54265
thealmightyderpybird <73889377+thealmighyderpybird@users.noreply.github.com>
2025-09-29 08:31:55 -0400

    iframe poll again

--- src/app/tool/poll/Redir.tsx
+++ src/app/tool/poll/Redir.tsx
@@ -5,7 +5,7 @@ import { useEffect } from "react";
 
 export default function PollClientSideRedirTool() {
     useEffect(() => {
-        /* fetch(CBSHServerURL + "/infofetch/daily-poll")
+        fetch(CBSHServerURL + "/infofetch/daily-poll")
             .then(r => r.json())
             // eslint-disable-next-line @typescript-eslint/no-unsafe-return,@typescript-eslint/no-unsafe-member-access
             .then(d => d.data)
@@ -13,10 +13,7 @@ export default function PollClientSideRedirTool() {
                 if (typeof url === "string") window.location.href = url;
                 else window.location.href = "/tool/poll/error"
             })
-            .catch(() => window.location.href = "/tool/poll/error"); */
-        window.open("https://docs.google.com/forms/d/e/1FAIpQLSdCrAq6MwApik7roO2LvmTy74Su2hSJ0UbHv3TVcFBn3qyQdA/viewform",
-            "daily-poll", "status=0,toolbar=0,location=0,menubar=0,scrollbars=1,height=800,width=600");
-        window.location.href = "/tool/poll/new-tab";
+            .catch(() => window.location.href = "/tool/poll/error");
     }, []);
 
     return null;
