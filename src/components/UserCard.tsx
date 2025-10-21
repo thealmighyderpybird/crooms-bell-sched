4425e68702e13715c177d3768e7712b87637e9c5
Andrew Jennings <73889377+thealmighyderpybird@users.noreply.github.com>
2025-10-21 14:12:56 -0400

    Update UserCard.tsx

--- src/components/UserCard.tsx
+++ src/components/UserCard.tsx
@@ -1,3 +1,5 @@
-export default function UserCard({ userDetails }) {
+import type User from "~/types/user";
+
+export default function UserCard({ userDetails }: { userDetails: User }) {
     return <div>{ JSON.stringify(userDetails) }</div>
 }
