7fcbffff35ee4ed10a9d71c0df65c2be49c337a3
Andrew Jennings <73889377+thealmighyderpybird@users.noreply.github.com>
2025-10-28 10:56:43 -0400

    bugfix for set is active

--- src/components/modals/ClientProwlerLock.tsx
+++ src/components/modals/ClientProwlerLock.tsx
@@ -4,5 +4,7 @@ import ProwlerLockDialog from "./ProwlerLock";
 import {createPortal} from "react-dom";
 
 export default function ClientProwlerLock({ allowed }: { allowed: boolean }) {
-    return !allowed && createPortal(<ProwlerLockDialog />, document.getElementById("modal-portal")!)
+    const setIsActive = (boolean: boolean) => boolean;
+
+    return !allowed && createPortal(<ProwlerLockDialog setIsActive={setIsActive} />, document.getElementById("modal-portal")!)
 }
