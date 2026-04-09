9787d9c8d13c0d42444f4123a18a717a009351a9
Andrew Jennings <73889377+thealmighyderpybird@users.noreply.github.com>
2026-04-09 10:15:01 -0400

    fix scroll issue

--- src/prowler/post/content.tsx
+++ src/prowler/post/content.tsx
@@ -5,20 +5,27 @@ import sanitizeContent from "~/lib/SanitizeContent";
 import styles from "~/prowler/prowler.module.css";
 
 export default function Content({ content }: { content: string }) {
+    const contentRef = useRef<HTMLDivElement>(null);
     const [needViewMore, setNeedViewMore] = useState(false);
     const [viewingMore, setViewingMore] = useState(false);
-    const contentRef = useRef(null);
 
     const isViewingMore = () => viewingMore ? ` ${styles.viewMore}` : "";
+    const x = () => {
+        if (contentRef.current !== null) {
+            setNeedViewMore(contentRef.current.clientHeight >= 200);
 
-    useEffect(() => {
-        // @ts-expect-error clientHeight works even though it isn't explicit
-        if (contentRef.current) setNeedViewMore(contentRef.current.clientHeight >= 200)
-    }, []);
+            for (let i = 0; i < contentRef.current.children.length; i++) {
+                if (contentRef.current.children.item(i)!.clientHeight >= 200) setNeedViewMore(true);
+            }
+        }
+    }
+
+
+    useEffect(() => x(), []);
 
     return <>
         <div className={ styles.corePostContent + isViewingMore() } dangerouslySetInnerHTML={{ __html: sanitizeContent(content) }}
-             ref={contentRef} />
+             ref={contentRef} onLoad={x} />
         { needViewMore && <span onClick={() => setViewingMore(!viewingMore)} className={ styles.viewMoreButton }>
             View { !viewingMore ? "More" : "Less" }</span> }
     </>;
