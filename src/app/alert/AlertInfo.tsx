f4deb71d47053128beea5f03df9710b6360e7cab
Andrew Jennings <73889377+thealmighyderpybird@users.noreply.github.com>
2026-09-04 19:51:04 -0400

    update alert page

--- src/app/alert/AlertInfo.tsx
+++ src/app/alert/AlertInfo.tsx
@@ -1,34 +1,17 @@
-"use client";
+'use client';
 
-import CardHeader from "~/components/index/CardHeader";
-import parseEndTime from "~/lib/parseEndTime";
-import { useRouter } from "next/navigation";
-import { useEffect, useState } from "react";
+import CardHeader from '~/components/index/CardHeader';
+import parseEndTime from '~/lib/parseEndTime';
 
-export default function AlertInfo({ alertId }: { alertId: string | undefined }) {
-    const [alertInfo, setAlertInfo] = useState({});
-    const router = useRouter();
-
-    useEffect(() => {
-        if (!alertId) router.push("/not-found");
-        fetch("https://api.weather.gov/alerts/" + alertId)
-            .then(r => r.json())
-            // eslint-disable-next-line @typescript-eslint/no-unsafe-argument,@typescript-eslint/no-unsafe-member-access
-            .then(r => setAlertInfo(r.properties))
-            .catch(() => router.push("/not-found"));
-        // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, []);
-
-    return alertInfo ? <>
-        { /* @ts-expect-error event, ends, and expires not included in generic type */ }
+export default function AlertInfo({ alertInfo }: { alertInfo: {
+    event: string, senderName: string, ends: string, expires: string, description: string
+}}) {
+    return <>
         <CardHeader>{ alertInfo.event }</CardHeader>
-        <ul className="p-0 list-none">
-            { /* @ts-expect-error event, ends, and expires not included in generic type */ }
+        <ul className='p-0 list-none'>
             <li><b>Issued by:</b> { alertInfo.senderName }</li>
-            {/* eslint-disable-next-line @typescript-eslint/no-unsafe-argument *//* @ts-expect-error not included in generic type */ }
             <li><b>Expires:</b> { parseEndTime(new Date(alertInfo.ends), new Date(alertInfo.expires)) }</li>
         </ul>
-        { /* @ts-expect-error event, ends, and expires not included in generic type */ }
-        <pre className="break-after-all mb-0 overflow-x-auto whitespace-pre">{ alertInfo.description }</pre>
-    </> : null;
+        <pre className='break-after-all mb-0 overflow-x-auto whitespace-normal'>{ alertInfo.description }</pre>
+    </>;
 }
--- src/app/alert/page.tsx
+++ src/app/alert/page.tsx
@@ -1,15 +1,23 @@
-import CardLayout from "~/components/index/CardLayout";
-import AlertInfo from "~/app/alert/AlertInfo";
-import Card from "~/components/index/Card";
+import CardLayout from '~/components/index/CardLayout';
+import ThemeProvider from '~/components/ThemeProvider';
+import AlertInfo from '~/app/alert/AlertInfo';
+import Card from '~/components/index/Card';
+import { notFound } from 'next/navigation';
 
-export default async function AlertsPage({ searchParams }: {
-    searchParams: Promise<{ id: string | undefined }>
-}) {
+export default async function AlertsPage({ searchParams }: { searchParams: Promise<{ id: string | undefined }> }) {
     const { id } = await searchParams;
 
-    return <CardLayout>
-        <Card>
-            <AlertInfo alertId={ id } />
-        </Card>
-    </CardLayout>;
+    if (!id) notFound();
+    try {
+        const r = await fetch(`https://api.weather.gov/alerts/${id}`);
+        const res = await r.json();
+
+        return <ThemeProvider>
+            <CardLayout>
+                <Card>
+                    <AlertInfo alertInfo={res.properties} />
+                </Card>
+            </CardLayout>
+        </ThemeProvider>;
+    } catch {notFound()}
 }
